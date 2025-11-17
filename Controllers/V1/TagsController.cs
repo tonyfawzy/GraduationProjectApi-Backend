33a2cdcffaf165d9352d6164fd595c78a54cca39
Tony <tony@tony.com>
2025-11-17 19:56:38 +0200

    Refresh

--- Controllers/V1/TagsController.cs
+++ Controllers/V1/TagsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace GraduationProjectApi.Controllers;
+namespace GraduationProjectApi.Controllers.V1;
 [ApiController]
 [Route("tags")]
 public class TagsController(ApplicationDbContext dbContext) : ControllerBase
