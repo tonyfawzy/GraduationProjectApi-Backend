8f87c2a776b5e6f65a955af3ddb7f61160ed33b3
Tony <tony@tony.com>
2025-11-17 18:55:01 +0200

    Refresh

---
+++ Controllers/V1/ServicesController.cs
@@ -0,0 +1,186 @@
+using GraduationProjectApi.DTOs;
+using GraduationProjectApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraduationProjectApi.Controllers.V1;
+
+[ApiController]
+[Route("test")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "API is working!" });
+    }
+}
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/services")]
+public class ServicesController(ApplicationDbContext dbContext) : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    /*
+        [HttpPost("delete")]
+        [Authorize]
+        public async Task<IActionResult> DeleteService()
+        {
+            var userIdClaim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                return Unauthorized(new { error = "Invalid token." });
+            }
+
+            var userId = userIdClaim.Value;
+
+
+        }
+   */
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> CreateServiceAsync([FromForm] CreateServiceRequestDto serviceDto)
+    {
+        var userIdClaim = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+            return Unauthorized(new { error = "Invalid token: missing user identifier claim." });
+
+        var userId = userIdClaim.Value;
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+        if (user == null)
+            return Unauthorized(new { error = "User not found for the provided token." });
+
+        if (user.IsSuspended)
+            return Unauthorized(new { error = "User account is suspended." });
+
+        if (serviceDto.ServiceImages != null && serviceDto.ServiceImages.Count > 5)
+            return BadRequest(new { error = "Maximum 5 images allowed." });
+
+
+/*
+        var tags = new List<Tag>();
+        if (serviceDto.TagIds != null && serviceDto.TagIds.Count > 0)
+        {
+            var foundTags = await _dbContext.Tags.Where(t => serviceDto.TagIds.Contains(t.TagId)).ToListAsync();
+            if (foundTags.Count != serviceDto.TagIds.Count)
+                return BadRequest(new { error = "One or more TagIds are invalid." });
+            tags.AddRange(foundTags);
+        }
+        */
+        var service = new Service
+        {
+            ServiceName = serviceDto.Title,
+            Description = serviceDto.Description,
+            UserId = user.UserId,
+        };
+
+        _dbContext.Services.Add(service);
+        await _dbContext.SaveChangesAsync();
+/*
+        if (tags.Count > 0)
+        {
+            foreach (var tag in tags.Take(10))
+            {
+                var st = new ServiceTag
+                {
+                    ServiceId = service.ServiceId,
+                    TagId = tag.TagId
+                };
+                _dbContext.ServiceTags.Add(st);
+            }
+            await _dbContext.SaveChangesAsync();
+        }
+*/
+        if (serviceDto.ServiceImages != null && serviceDto.ServiceImages.Any())
+        {
+             var folderPath = Path.Combine("wwwroot", "service", service.ServiceId, "images");   
+             if (!Directory.Exists(folderPath))
+             {
+                 Directory.CreateDirectory(folderPath);
+             }
+
+             foreach (var image in serviceDto.ServiceImages)
+             {
+                 if (image.Length > 0)
+                 {
+                     var fileExtension = Path.GetExtension(image.FileName);
+                     var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+                     if (!allowedExtensions.Contains(fileExtension.ToLower()))
+                     {
+                         return BadRequest(new { error = "Invalid image format. Allowed formats: jpg, jpeg, png." });
+                     }
+
+                     var uniqueFileName = $"{Guid.NewGuid()}{fileExtension}";
+                     var filePath = Path.Combine(folderPath, uniqueFileName);
+
+                     using (var stream = new FileStream(filePath, FileMode.Create))
+                     {
+                         await image.CopyToAsync(stream);
+                     }
+
+                     var serviceImage = new ServiceImage
+                     {
+                         ServiceId = service.ServiceId,
+                         ServiceImageUrl = $"/service/{service.ServiceId}/images/{uniqueFileName}"
+                     };
+                     _dbContext.ServiceImages.Add(serviceImage);
+                 }
+             }
+        }
+
+        await _dbContext.SaveChangesAsync();
+        return Ok(new
+        {
+            message = "Service added successfully.",
+            ServiceId = service.ServiceId
+        });
+    }
+    
+    [HttpGet("{serviceId}")]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetServiceById([FromRoute] string serviceId)
+    {
+        var service = await _dbContext.Services
+            .Include(s => s.User)
+                .ThenInclude(a => a.Address)
+            .Include(s => s.ServiceImages)
+            .FirstOrDefaultAsync(s => s.ServiceId == serviceId);
+
+        if (service == null)
+        {
+            return NotFound(new { error = "Service not found." });
+        }
+
+        return Ok(new
+        {
+            message = "Service retrieved successfully.",
+            data = new
+            {
+            service.ServiceId,
+            service.ServiceName,
+            service.Description,
+            service.Rating,
+            
+            User = new
+            {
+                service.User.UserId,
+                service.User.Fullname,
+                service.User.ProfileImage,
+                service.User.PhoneNumber,
+                service.User.Bio,
+                address = service.User.Address.Governorate
+            },
+            serviceImages = service.ServiceImages.Select(img => img.ServiceImageUrl).ToList(),
+
+            service.CreatedAt
+            }
+
+
+        });
+    }
+}
+
---
+++ Controllers/V1/UsersController.cs
@@ -0,0 +1,400 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using GraduationProjectApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using GraduationProjectApi.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using YamlDotNet.Core.Tokens;
+using System.Net.Http.Headers;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+
+namespace GraduationProjectApi.Controllers.V1;
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/auth")]
+public class AuthController(JwtOptions jwtOptions, ApplicationDbContext dbContext) : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterAsync([FromBody] RegisterUserDto RegDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var isPhoneNumberExists = await _dbContext.Users.AnyAsync(u =>
+            u.PhoneNumber == RegDto.PhoneNumber || u.SecondPhoneNumber == RegDto.PhoneNumber);
+
+        if (isPhoneNumberExists)
+            return BadRequest(new
+            {
+                error = "Phone number already in use."
+            });
+
+        var user = new User
+        {
+            Fullname = RegDto.Fullname,
+            PhoneNumber = RegDto.PhoneNumber,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(RegDto.Password),
+            Gender = string.IsNullOrWhiteSpace(RegDto.Gender) ? null : RegDto.Gender,
+            DateOfBirth = RegDto.DateOfBirth,
+            Email = string.IsNullOrWhiteSpace(RegDto.Email) ? null : RegDto.Email,
+            NormalizedEmail = string.IsNullOrWhiteSpace(RegDto.Email) ? null : RegDto.Email.ToUpperInvariant(),
+            Address = new Address
+            {
+                Governorate = RegDto.Address
+            }
+        };
+
+        _dbContext.Users.Add(user);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new
+        {
+            message = "User registered successfully."
+        });
+    }
+
+    [HttpPost]
+    [Route("login")]
+    public async Task<IActionResult> LoginAsync([FromBody] LoginUserDto LogDto)
+    {
+
+        var user = await _dbContext.Users
+        .Include(a => a.Address)
+        .FirstOrDefaultAsync(u => u.PhoneNumber == LogDto.PhoneNumber);
+
+        if (user == null || !BCrypt.Net.BCrypt.Verify(LogDto.Password, user.PasswordHash))
+        {
+            return Unauthorized(new { error = "Invalid phone number or password." });
+        }
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Issuer = jwtOptions.Issuer,
+            Audience = jwtOptions.Audience,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtOptions.Key)), SecurityAlgorithms.HmacSha256Signature),
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new (ClaimTypes.Name, user.Fullname),
+                new (ClaimTypes.Role, user.Permission == 0 ? "User" : "Admin")
+            })
+        };
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+        var accessToken = tokenHandler.WriteToken(securityToken);
+
+        var userDto = new
+        {
+            userId = user.UserId,
+            fullname = user.Fullname,
+            phoneNumber = user.PhoneNumber,
+            secondPhoneNumber = user.SecondPhoneNumber,
+            email = user.Email,
+            role = (user.Permission == 0) ? "User" : "Admin",
+            gender = user.Gender,
+            dateOfBirth = user.DateOfBirth,
+            address = user.Address.Governorate,
+            profileImage = user.ProfileImage,
+            bio = user.Bio,
+            balance = user.Balance,
+            isSuspended = user.IsSuspended,
+            //services = user.Services
+
+            
+            createdAt = user.CreatedAt,
+        };
+
+        return Ok(new
+        {
+            message = "Login successful",
+            user = userDto,
+            token = accessToken
+        });
+    }
+}
+
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/v{version:apiVersion}/users")]
+public class UsersController(JwtOptions jwtOptions, ApplicationDbContext dbContext) : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext = dbContext;
+
+    [HttpGet]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetAllUsersAsync()
+    {
+        var users = await _dbContext.Users
+            .Include(u => u.Services)
+                .ThenInclude(s => s.ServiceTags)
+                    .ThenInclude(st => st.Tag)
+            .Include(a => a.Address)
+            .ToListAsync();
+
+        var result = users.Select(user => new
+        {
+            user.UserId,
+            user.Fullname,
+            user.PhoneNumber,
+            user.Email,
+            user.DateOfBirth,
+            user.Bio,
+            user.ProfileImage,
+            user.Address.Governorate,
+            user.Balance,
+            Services = user.Services.Select(service => new
+            {
+                service.ServiceId,
+                service.ServiceName,
+                service.Description,
+                ServiceTags = (service.ServiceTags ?? Enumerable.Empty<ServiceTag>())
+                    .Select(st => new
+                    {
+                        st.TagId,
+                        tagName = st.Tag != null ? st.Tag.TagName : null
+                    })
+            })
+        });
+
+        return Ok(result);
+    }
+
+    [HttpGet("{userId}")]
+    //[Authorize(Roles = "Admin")]
+    public async Task<IActionResult> GetUserByIdAsync([FromRoute] string userId)
+    {
+        var user = await _dbContext.Users
+            .Include(u => u.Services)
+                .ThenInclude(s => s.ServiceTags)
+                    .ThenInclude(st => st.Tag)
+            .Include(a => a.Address)
+            .FirstOrDefaultAsync(u => u.UserId == userId);
+
+        if (user == null)
+        {
+            return NotFound(new { error = "User not found." });
+        }
+
+        var result = new
+        {
+            user.UserId,
+            user.Fullname,
+            user.PhoneNumber,
+            user.Email,
+            user.DateOfBirth,
+            user.Bio,
+            user.ProfileImage,
+            address = user.Address.Governorate,
+            Services = user.Services.Select(service => new
+            {
+                service.ServiceId,
+                service.ServiceName,
+                service.Description,
+                ServiceTags = (service.ServiceTags ?? Enumerable.Empty<ServiceTag>())
+                    .Select(st => new
+                    {
+                        st.TagId,
+                        tagName = st.Tag != null ? st.Tag.TagName : null
+                    })
+            })
+        };
+
+        return Ok(
+            new
+            {
+                message = "Success",
+                data = result
+            });
+    }
+
+/*
+    [HttpGet("getInfo")]
+    [Authorize]
+    public async Task<IActionResult> GetMyInfoAsync()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+        {
+            return Unauthorized(new { error = "Invalid token." });
+        }
+
+        var userId = userIdClaim.Value;
+
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+        if (user == null)
+        {
+            return NotFound(new
+            {
+                error = "User not found."
+            });
+        }
+
+        var result = new
+        {
+            userId = user.UserId,
+            fullName = user.Fullname,
+            phoneNumber = user.PhoneNumber,
+            secondPhoneNumber = user.SecondPhoneNumber,
+            permission = (user.Permission == 0) ? "User" : "Admin",
+            gender = user.Gender,
+            dateOfBirth = user.DateOfBirth,
+            profileImage = user.ProfileImage,
+            bio = user.Bio,
+            email = user.Email,
+            isSuspended = user.IsSuspended,
+            createdAt = user.CreatedAt
+        };
+
+        return Ok(new
+        {
+            message = "Success",
+            data = result
+        });
+    }
+*/
+    [HttpPatch]
+    [Authorize]
+    public async Task<IActionResult> UpdateProfileAsync([FromForm] UpdateUserInfoDto updateDto, [FromForm] IFormFile? ProfileImage)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+        {
+            return Unauthorized(new { error = "Invalid token." });
+        }
+
+        var userId = userIdClaim.Value;
+
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+        if (user == null)
+        {
+            return NotFound(new { error = "User not found." });
+        }
+
+        if (!string.IsNullOrWhiteSpace(updateDto.Fullname))
+        {
+            if (updateDto.Fullname.Length < 3 || updateDto.Fullname.Length > 70)
+            {
+                return BadRequest(new { error = "Fullname must be between 3 and 70 characters." });
+            }
+            user.Fullname = updateDto.Fullname;
+        }
+
+        if (!string.IsNullOrWhiteSpace(updateDto.Bio))
+        {
+            if (updateDto.Bio.Length > 250)
+            {
+                return BadRequest(new { error = "Bio cannot exceed 250 characters." });
+            }
+            user.Bio = updateDto.Bio;
+        }
+
+        if (!string.IsNullOrWhiteSpace(updateDto.SecondPhoneNumber))
+        {
+            if (updateDto.SecondPhoneNumber.Length > 15)
+            {
+                return BadRequest(new { error = "Second phone number cannot exceed 15 characters." });
+            }
+            user.SecondPhoneNumber = updateDto.SecondPhoneNumber;
+        }
+
+        if (ProfileImage != null)
+        {
+            /*
+            var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", "pfps");
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+            var fileName = $"{Guid.NewGuid()}";
+            var filePath = Path.Combine(uploadsFolder, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await ProfileImage.CopyToAsync(stream);
+            }
+
+            user.ProfileImageUrl = $"/uploads/pfps/{fileName}";
+            */
+
+/*
+        using (var ms = new MemoryStream())
+        {
+            await ProfileImage.CopyToAsync(ms);
+            var fileBytes = ms.ToArray();
+            string base64String = Convert.ToBase64String(fileBytes);
+
+            user.ProfileImageUrl = "data:image/jpeg;base64," + base64String;
+        }
+*/
+
+            using var image = await SixLabors.ImageSharp.Image.LoadAsync(ProfileImage.OpenReadStream());
+
+            image.Mutate(x => x.Resize(new SixLabors.ImageSharp.Processing.ResizeOptions
+            {
+                Size = new SixLabors.ImageSharp.Size(128, 128),
+                Mode = SixLabors.ImageSharp.Processing.ResizeMode.Max,
+            }));
+
+            image.Mutate(x => x.Pad(128, 128, SixLabors.ImageSharp.Color.White));
+
+            using var ms = new MemoryStream();
+            await image.SaveAsJpegAsync(ms);
+
+            string base64String = Convert.ToBase64String(ms.ToArray());
+            user.ProfileImage = "data:image/jpeg;base64," + base64String;
+
+        }
+
+        _dbContext.Users.Update(user);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new
+        {
+            message = "Profile updated successfully.",
+            Fullname = user.Fullname,
+            Bio = user.Bio,
+            SecondPhoneNumber = user.SecondPhoneNumber,
+            ProfileImage = user.ProfileImage
+        });
+    }
+
+    [HttpDelete]
+    [Authorize]
+    public async Task<IActionResult> DeleteUserAsync()
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        if (userIdClaim == null)
+        {
+            return Unauthorized(new { error = "Invalid token." });
+        }
+
+        var userId = userIdClaim.Value;
+
+        var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+        if (user == null)
+        {
+            return NotFound(new
+            {
+                message = "User not found."
+            });
+        }
+
+        _dbContext.Users.Remove(user);
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new
+        {
+            message = "Your account has been deleted successfully."
+        });
+    }
+
+}
